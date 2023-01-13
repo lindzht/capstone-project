ebd10272e082bea5e55c0945ac8b71b68055a185
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-01-12 23:43:14 -0500

    started adding styles to nav bar and landing page. added hamburger icon as a tester

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,24 @@
+import { Icon } from "semantic-ui-react";
+import { useState } from "react";
+
 function Nav (){
+
+    const [expandNav, setExpandNav] = useState(false)
+
+    const handleHamburger = ()=> {
+        setExpandNav(!expandNav);
+    }
+
+if (!expandNav) return <Icon id="hamburger" name="star" size="big" onClick={handleHamburger}/>
     return(
         <div id="nav-container">
-            <h1> this is the nav </h1>
+            <Icon id="hamburger" name="star" size="big" onClick={handleHamburger} />
+            <div id="nav-content">
+                <h3> This is a link </h3>
+                <h3> This is a link </h3>
+                <h3> This is a link </h3>
+                <h3> This is a link link link link</h3>
+            </div>
         </div>
     )
 }
