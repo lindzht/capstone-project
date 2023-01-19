b3c5eee6bb2ce6d3de06b28d139f9c1bbab55a78
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-01-19 15:29:11 -0500

    fixed team reqs rendering when clicking on new admin team card

--- client/src/components/TeamDashboardHome.js
+++ client/src/components/TeamDashboardHome.js
@@ -1,10 +1,13 @@
 import { Icon } from 'semantic-ui-react';
+import { useParams, } from 'react-router-dom';
 import { useState } from "react";
 import TeamReqs from "./TeamReqs";
 import TeamRecruiterCard from "./TeamRecruiterCard";
 import AddRecruiterCard from './AddRecruiterCard';
 
 function TeamDashboardHome({currentUser, currentTeam, deleteRecruiterFromTeam}) {
+    let params = useParams();
+    console.log(params);
 
     function renderOpenReqTable (){
         return(<TeamReqs currentTeam={currentTeam}/>)
@@ -31,35 +34,13 @@ function TeamDashboardHome({currentUser, currentTeam, deleteRecruiterFromTeam}) 
     }
 
 
+    console.log(currentTeam)
 
 
     return(
         <div id="team-container">
             <div id="team-container-left">
-                {/* <div className="dashboard-content-container">
-                    <div id="dashboard-content-middle">
-                        <div id="my-metrics-card-container">
-                            <h1>{!currentTeam ? "Loading..." : currentTeam.open_reqs}</h1>
-                            <h3>Open Reqs</h3>
-                        </div>
-                    </div>
-
-                    <div id="dashboard-content-middle">
-                        <div id="my-metrics-card-container">   
-                            <h1>{!currentTeam ? "Loading..." : currentTeam.hired_reqs}</h1>
-                            <h3>Hired Reqs</h3>
-                        </div>
-                    </div>
-                    
-                    <div id="dashboard-content-right">
-                        <div id="my-metrics-card-container">
-                            <h3>Metrics</h3>
-                        </div>
-                    </div>
-                </div> */}
-                    {!currentTeam  ?      
-                        <h3>"Loading..."</h3>
-                        : 
+                    {currentTeam && currentTeam.id == params.teamId ?      
                         <div className='req-container'>
                             <div id="req-top-container">
                                 <h1>Open Reqs</h1>
@@ -70,6 +51,8 @@ function TeamDashboardHome({currentUser, currentTeam, deleteRecruiterFromTeam}) 
                             {renderOpenReqTable()}
                             {/* <TeamDashboardHome /> */}
                         </div>
+                        : 
+                        <h3>"Loading..."</h3>
                     }
             </div>
             <div id="team-container-right">
@@ -99,7 +82,7 @@ function TeamDashboardHome({currentUser, currentTeam, deleteRecruiterFromTeam}) 
                             </div>
                         }
                 </div>
-                
+
             </div>
 
         </div>
