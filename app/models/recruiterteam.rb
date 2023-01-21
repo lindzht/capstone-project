455d64b57078315931a8f32f1a007251cf9c30a4
Lindsay Taylor <llindsayttaylor@gmail.com>
2023-01-21 17:40:52 -0500

    Recruiter can now only be added to a team board once

--- app/models/recruiterteam.rb
+++ app/models/recruiterteam.rb
@@ -1,4 +1,8 @@
 class Recruiterteam < ApplicationRecord
   belongs_to :recruiter
   belongs_to :team
+
+  validates :recruiter_id, uniqueness: { scope: :team_id, 
+  message: "You already got this recruiter on this team yo!" }
+
 end
